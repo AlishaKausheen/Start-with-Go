ab74351eb2116a43b22df697f6cd463c3c696351
AlishaKausheen <alishakausheen@gmail.com>
2024-11-15 21:16:45 +0530

    struct

---
+++ 16_structs/structs.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	
+	"time"
+)
+
+//order struct
+type order struct{
+	id string
+	amount float32
+	status string
+	createdAt time.Time //nano sec precision
+}
+
+//func to change status
+//receiver type 
+func (o *order) changeStatus(status string){
+o.status =status
+}
+
+func (o order) getAmount() float32{
+	return o.amount
+	}
+
+	//constructor
+	func newOrder(id string,amount float32, status string) *order{
+		//initial setup goes here
+		myOrder3 := order{
+			id: id,
+			amount: amount,
+			status: status,
+		
+		 }
+		 return &myOrder3
+	}
+
+func main(){
+
+ myOrder := order{
+	id: "1",
+	amount: 50.00,
+	status: "received",
+
+ }
+ myOrder2 := order{
+	id: "2",
+	amount: 500,
+	status: "delevered",
+	createdAt: time.Now(),
+ }
+ myOrder.createdAt =time.Now()
+
+ myOrder.changeStatus("confirmed")
+
+
+ fmt.Println(myOrder.id)
+ fmt.Println(myOrder)
+ fmt.Println(myOrder2)
+
+ myOrder3:=newOrder("3",30.50, "recieved")
+ fmt.Println(myOrder3)
+
+
+ //struct without name
+
+ language := struct{
+	name string
+	isGood bool
+ }{"golang",true}
+
+ fmt.Println(language)
+}
